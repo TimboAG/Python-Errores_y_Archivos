a80d23930881c3652e3688af159ca6f939576b73
Alejandra Guerin <79866676+TimboAG@users.noreply.github.com>
2023-04-26 14:47:44 -0300

    aprendiendo python

---
+++ ExcepcionesTryExcept/excepcionMultiple.py
@@ -0,0 +1,15 @@
+division = 0
+try: 
+    a = int(input('Ingrese el Dividendo: '))
+    b = int(input('Ingrese el Divisor: '))
+
+    division = a / b
+except ValueError:
+    print('Error: Ingrese solo números enteros!')
+except ZeroDivisionError:
+    print('Error: No se Puede divir por cero!')
+except Exception as error:
+    #print('Ha ocurrido error no previsto:', type(error).__name__)
+    print('Ha ocurrido error no previsto:', error)
+    
+print('La división es: ', division)
